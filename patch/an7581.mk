3a6405a2685370c6220c56ea0c5845e0519cb4f3
oyk470p <147223649+oyk470p@users.noreply.github.com>
2026-06-01 21:37:39 +0800

    Update an7581.mk

--- patch/an7581.mk
+++ patch/an7581.mk
@@ -12,6 +12,33 @@ define Build/an7581-bl31-uboot
   cat $(STAGING_DIR_IMAGE)/an7581_$1-bl31-u-boot.fip >> $@
 endef
 
+define Build/an7581-chainloader
+  $(INSTALL_DIR) $(KDIR)/chainload-fit-$(notdir $@)
+  @if [ -f "$(STAGING_DIR_IMAGE)/an7581_$1-u-boot.bin.lzma" ]; then \
+    KERNEL="$(STAGING_DIR_IMAGE)/an7581_$1-u-boot.bin.lzma"; \
+    COMP="lzma"; \
+  else \
+    KERNEL="$(STAGING_DIR_IMAGE)/an7581_$1-u-boot.bin"; \
+    COMP="none"; \
+  fi; \
+  $(TOPDIR)/scripts/mkits.sh \
+    -D $(DEVICE_NAME) \
+    -o $(KDIR)/chainload-fit-$(notdir $@)/u-boot.its \
+    -k $$KERNEL \
+    -C $$COMP \
+    -a 0x80200000 -e 0x80200000 \
+    -c conf-uboot \
+    -A arm64 -v u-boot \
+    -d $(STAGING_DIR_IMAGE)/an7581_$1-u-boot.dtb \
+    -s 0x82000000
+  PATH=$(LINUX_DIR)/scripts/dtc:$(PATH) \
+    $(STAGING_DIR_HOST)/bin/mkimage \
+    -D "-i $(KDIR)/chainload-fit-$(notdir $@)" \
+    -f $(KDIR)/chainload-fit-$(notdir $@)/u-boot.its \
+    $(STAGING_DIR_IMAGE)/an7581_$1-chainload-u-boot.itb
+  cat $(STAGING_DIR_IMAGE)/an7581_$1-chainload-u-boot.itb >> $@
+endef
+
 define Device/FitImageLzma
 	KERNEL_SUFFIX := -uImage.itb
 	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
@@ -59,6 +86,6 @@ define Device/bell_xg-040g-md
   IMAGES := factory.bin sysupgrade.bin
   IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  DEVICE_PACKAGES := kmod-phy-airoha-en8811h kmod-i2c-an7581 kmod-leds-gpio kmod-gpio-button-hotplug uboot-envtools ubi-utils kmod-usb-storage-uas kmod-usb-net-cdc-ether kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 kmod-fs-ntfs3 blkid lsblk
+  DEVICE_PACKAGES := airoha-en7581-npu-firmware kmod-phy-airoha-en8811h kmod-i2c-an7581 kmod-leds-gpio kmod-gpio-button-hotplug uboot-envtools ubi-utils kmod-usb-storage-uas kmod-usb-net-cdc-ether kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 kmod-fs-ntfs3 blkid lsblk
 endef
 TARGET_DEVICES += bell_xg-040g-md
